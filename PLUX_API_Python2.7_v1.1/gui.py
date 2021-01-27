f6e7e41b1feed2dd9a21bf3974bd625ae6664b37
jesterx7 <marcellinoagustinus@ymail.com>
2021-01-27 09:45:59 +0700

    change button size to ratio

--- PLUX_API_Python2.7_v1.1/gui.py
+++ PLUX_API_Python2.7_v1.1/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font as tkFont
 import threading
 import time
 from pynput import keyboard
@@ -150,9 +151,16 @@ def showFrame(frame_list, selected):
         clearFrame(frame[1])
 
 def showGraph(frame, graph_btn):
+<<<<<<< Updated upstream
     graph_img = ImageTk.PhotoImage(resize_icon('emg_signal.png', 512, 223))
     graph = make_label(frame, (GetSystemMetrics(0) - 250) / 2 - 300, (GetSystemMetrics(1) - 150) / 2 + 50,
                512, 223, text="Result : ", bg="#ffffff", fg="black", font="Bahnschrift 16 bold", anchor=tk.W,
+=======
+    graph_img = ImageTk.PhotoImage(resize_icon('emg_signal.png', 612, 323))
+    global button_ratio
+    graph = make_label(frame, (GetSystemMetrics(0) - 250) / 2 - 300, (GetSystemMetrics(1) - 150) / 2 + 20,
+               612, 323, text="Result : ", bg="#ffffff", fg="black", font="Bahnschrift " + str(int(button_ratio * 16)) + " bold", anchor=tk.W,
+>>>>>>> Stashed changes
                image=graph_img)
     graph.photo = graph_img
     
@@ -169,7 +177,7 @@ def destroyGraph(frame, graph, graph_btn):
     
 
 def createHomePage():
-    global main_frame, home_button
+    global main_frame, home_button, button_ratio
     clearFrame(main_frame)
     main_frame = make_frame(window, 220, 60, GetSystemMetrics(0) - 250, GetSystemMetrics(1) - 150, bg="#ffffff")
     click_frame = make_frame(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2), (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100),
@@ -178,16 +186,25 @@ def createHomePage():
                (GetSystemMetrics(0) - 250) * 3 / 100, (GetSystemMetrics(0) - 250) * 3 / 100, bg="#ffd04f")
     make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 + ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + ((GetSystemMetrics(0) - 250) * 3 / 100) + 10, ((GetSystemMetrics(1) - 150) / 2) - ((GetSystemMetrics(1) - 150) * 43 / 100),
                (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Idle / No Click", bg="#ffffff", fg="black", 
+<<<<<<< Updated upstream
                font="Bahnschrift 12", anchor=tk.W)
+=======
+               font="Bahnschrift " + str(int(button_ratio * 14)) , anchor=tk.W)
+>>>>>>> Stashed changes
     make_frame(main_frame, (GetSystemMetrics(0) - 250) / 2 + ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + 10, ((GetSystemMetrics(1) - 150) / 2) + 10 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(0) - 250) * 3 / 100),
                (GetSystemMetrics(0) - 250) * 3 / 100, (GetSystemMetrics(0) - 250) * 3 / 100, bg="#fc643a")
     make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 + ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + ((GetSystemMetrics(0) - 250) * 3 / 100) + 10, ((GetSystemMetrics(1) - 150) / 2) + 10 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(0) - 250) * 3 / 100),
                (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Left Click", bg="#ffffff", fg="black", 
+<<<<<<< Updated upstream
                font="Bahnschrift 12", anchor=tk.W)
+=======
+               font="Bahnschrift " + str(int(button_ratio * 14)), anchor=tk.W)
+>>>>>>> Stashed changes
     make_frame(main_frame, (GetSystemMetrics(0) - 250) / 2 + ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + 10, ((GetSystemMetrics(1) - 150) / 2) + 20 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(0) - 250) * 3 / 100) * 2,
                (GetSystemMetrics(0) - 250) * 3 / 100, (GetSystemMetrics(0) - 250) * 3 / 100, bg="#3ad5fc")
     make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 + ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + ((GetSystemMetrics(0) - 250) * 3 / 100) + 10, ((GetSystemMetrics(1) - 150) / 2) + 20 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(0) - 250) * 3 / 100) * 2,
                (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Right Click", bg="#ffffff", fg="black", 
+<<<<<<< Updated upstream
                font="Bahnschrift 12", anchor=tk.W)
     run_status = make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2), (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + 10,
                (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Status : ", bg="#ffffff", fg="black", 
@@ -195,25 +212,34 @@ def createHomePage():
     run_result = make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2), (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + ((GetSystemMetrics(1) - 150) * 3 / 100) + 40,
                (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Result : ", bg="#ffffff", fg="black", 
                font="Bahnschrift 14 bold", anchor=tk.W)
+=======
+               font="Bahnschrift " + str(int(button_ratio * 14)), anchor=tk.W)
+    run_status = make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2), (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + 10,
+               (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Status : ", bg="#ffffff", fg="black", 
+               font="Bahnschrift " + str(int(button_ratio * 16)) + " bold", anchor=tk.W)
+    run_result = make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2), (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + ((GetSystemMetrics(1) - 150) * 3 / 100) + 40,
+               (GetSystemMetrics(0) - 250) * 12 / 100, (GetSystemMetrics(1) - 150) * 3 / 100, text="Result : ", bg="#ffffff", fg="black", 
+               font="Bahnschrift " + str(int(button_ratio * 16)) + " bold", anchor=tk.W)
+>>>>>>> Stashed changes
     make_label(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + ((GetSystemMetrics(0) - 250) * 36 / 100) - ((GetSystemMetrics(0) - 250) / 10) - ((GetSystemMetrics(0) - 250) / 10) + 20, (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + 10,
-                (GetSystemMetrics(0) - 250) / 10, (GetSystemMetrics(1) - 150) * 3 / 100, text='Method', font="Bahnschrift 14", bg='#ffffff', anchor=tk.W)
+                (GetSystemMetrics(0) - 250) / 10, (GetSystemMetrics(1) - 150) * 3 / 100, text='Method', font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", bg='#ffffff', anchor=tk.W)
     cb_method = make_comboBox(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + ((GetSystemMetrics(0) - 250) * 36 / 100) - ((GetSystemMetrics(0) - 250) / 10) + 20, (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + 10, 
-                (GetSystemMetrics(0) - 250) / 10, (GetSystemMetrics(1) - 150) * 3 / 100, font="Bahnschrift 14", state='readonly')
+                (GetSystemMetrics(0) - 250) / 10, (GetSystemMetrics(1) - 150) * 3 / 100, font="Bahnschrift " + str(int(button_ratio * 14)), state='readonly')
     cb_method['values'] = ('KNN', 'SVM')
     cb_method.current(0)
     graph_button = make_button(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2), (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + ((GetSystemMetrics(1) - 150) * 3 / 100) + ((GetSystemMetrics(1) - 150) * 3 / 100) + 80, (GetSystemMetrics(0) - 250) / 10,
             (GetSystemMetrics(1) - 150) * 5 / 100, text='Show Graph',
-            font="Bahnschrift 14 bold", border=0, bg='#d9d2d2', 
+            font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", border=0, bg='#d9d2d2', 
             compound=tk.CENTER, fg='#ffffff', activebackground='#018c49',
             state='disabled', command=lambda:showGraph(main_frame, graph_button))
     run_button = make_button(main_frame, (GetSystemMetrics(0) - 250) / 2 - ((GetSystemMetrics(0) - 250) * 36 / 100 / 2) + ((GetSystemMetrics(0) - 250) * 36 / 100) - ((GetSystemMetrics(0) - 250) / 10) + 20, (GetSystemMetrics(1) - 150) / 2 - ((GetSystemMetrics(1) - 150) * 43 / 100) + ((GetSystemMetrics(1) - 150) * 27 / 100) + ((GetSystemMetrics(1) - 150) * 3 / 100) + 40,
-            (GetSystemMetrics(0) - 250) / 10, (GetSystemMetrics(1) - 150) * 5 / 100, text='Run', font="Bahnschrift 14 bold", 
+            (GetSystemMetrics(0) - 250) / 10, (GetSystemMetrics(1) - 150) * 5 / 100, text='Run', font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", 
             border=0, bg='#6b84ff', compound=tk.CENTER, fg='#ffffff', activebackground='#3246a8', 
             command=lambda:runCallBack(click_frame, run_status, run_result, run_button, graph_button, cb_method.get()))
     
 
 def createAccuracyPage():
-    global main_frame
+    global main_frame, button_ratio
     clearFrame(main_frame)
     main_frame = make_frame(window, 220, 60, GetSystemMetrics(0) - 250, GetSystemMetrics(1) - 150, bg="#ffffff")
     knn_table_frame = make_frame(main_frame, 10, 50, 200, 30, bg="#ffffff")
@@ -223,7 +249,7 @@ def createAccuracyPage():
     total_columns = len(table_list[0])
     
     make_label(main_frame, 10, 10, 200, 30, text="KNN Method", bg="#ffffff", fg="black", 
-               font="Bahnschrift 14", anchor=tk.W)
+               font="Bahnschrift " + str(int(button_ratio * 14)), anchor=tk.W)
     
     for i in range(total_rows):
         for j in range(total_columns):
@@ -233,7 +259,7 @@ def createAccuracyPage():
             
     svm_table_frame = make_frame(main_frame, 10, 190, 200, 30, bg="#ffffff")        
     make_label(main_frame, 10, 150, 200, 30, text="SVM Method", bg="#ffffff", fg="black", 
-               font="Bahnschrift 14", anchor=tk.W)
+               font="Bahnschrift " + str(int(button_ratio * 14)), anchor=tk.W)
     
     for i in range(total_rows):
         for j in range(total_columns):
@@ -243,19 +269,19 @@ def createAccuracyPage():
 
 
 def createInformationPage():
-    global main_frame
+    global main_frame, button_ratio
     clearFrame(main_frame)
     main_frame = make_frame(window, 220, 60, GetSystemMetrics(0) - 250, GetSystemMetrics(1) - 150, bg="#ffffff")
                             
     make_label(main_frame, 10, 10, 200, 30, text="How System Works ?", bg="#ffffff", fg="black", 
-               font="Bahnschrift 16 bold", anchor=tk.W)
+               font="Bahnschrift " + str(int(button_ratio * 16)) + " bold", anchor=tk.W)
     img = ImageTk.PhotoImage(resize_icon('../Images/system_flow.jpg', 800, 80))
     system_img = make_label(main_frame, 10, 60, 800, 80, bg="#ffffff", fg="black", 
-               font="Bahnschrift 14", anchor=tk.W, image=img)
+               font="Bahnschrift " + str(int(button_ratio * 14)), anchor=tk.W, image=img)
     system_img.photo = img
     info_file = open('info.txt', 'r')
     make_label(main_frame, 10, 140, 870, 300, text=info_file.read(), bg="#ffffff", fg="black", 
-               font="Bahnschrift 14", anchor=tk.CENTER, justify=tk.LEFT)
+               font="Bahnschrift " + str(int(button_ratio * 14)), anchor=tk.CENTER, justify=tk.LEFT)
                             
 
 window = tk.Tk() #window polos
@@ -263,36 +289,41 @@ window.title("Click Classification")
 window.geometry(str(GetSystemMetrics(0)) + 'x' + str(GetSystemMetrics(1))) #resolusi
 window.configure(bg="#f2f2f2") #warna background
 
+<<<<<<< Updated upstream
 checkClick = False #
+=======
+checkClick = False
+button_ratio = GetSystemMetrics(0)/1920;
+>>>>>>> Stashed changes
 run_button = tk.Button()
 main_frame = make_frame(window, 220, 60, GetSystemMetrics(0) - 250, GetSystemMetrics(1) - 150, bg="#ffffff")
 make_label(window, 0, 0, GetSystemMetrics(0), 50, text="Click Classification", 
-           bg="#3246a8", fg="white", font="Corbel 16 bold", anchor=tk.W, padx=10)
+           bg="#3246a8", fg="white", font="Corbel " + str(int(button_ratio * 16)) + " bold", anchor=tk.W, padx=10)
            
 make_frame(window, 0, 50, 200, GetSystemMetrics(1), bg="#ffffff")
            
 home_icon = ImageTk.PhotoImage(resize_icon('../Images/home.png', 20, 20))
 home_button = make_button(window, 0, 100, 200, 50, text=' Home', image=home_icon,
-            font="Bahnschrift 14 bold", border=0, bg='white', compound=tk.LEFT,
+            font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", border=0, bg='white', compound=tk.LEFT,
             anchor=tk.W, padx=25, activebackground='#6b84ff',
             command=lambda:createHomePage()).pack(fill=tk.BOTH, expand=1)
             
 accuracy_icon = ImageTk.PhotoImage(resize_icon('../Images/acc.png', 30, 30))
 accuracy_button = make_button(window, 0, 170, 200, 50, text='Accuracy', image=accuracy_icon,
-            font="Bahnschrift 14 bold", border=0, bg='white', compound=tk.LEFT,
+            font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", border=0, bg='white', compound=tk.LEFT,
             anchor=tk.W, padx=20, activebackground='#6b84ff',
             command=lambda:createAccuracyPage()).pack(fill=tk.BOTH, expand=1)
             
 lamp_icon = ImageTk.PhotoImage(resize_icon('../Images/lamp.jpg', 30, 30))
 info_button = make_button(window, 0, 240, 200, 50, text='Information', image=lamp_icon,
-            font="Bahnschrift 14 bold", border=0, bg='white', compound=tk.LEFT,
+            font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", border=0, bg='white', compound=tk.LEFT,
             anchor=tk.W, padx=20, activebackground='#6b84ff',
             command=lambda:createInformationPage())
 
 star_icon = ImageTk.PhotoImage(resize_icon('../Images/star.png', 20, 20))
-credit_button = make_button(window, 0, 310, 200, 50, text='Credit', image=star_icon,
-            font="Bahnschrift 14 bold", border=0, bg='white', compound=tk.LEFT,
-            anchor=tk.W, padx=25, activebackground='#6b84ff').pack(fill=tk.BOTH, expand=1)
+# credit_button = make_button(window, 0, 310, 200, 50, text='Credit', image=star_icon,
+#             font="Bahnschrift " + str(int(button_ratio * 14)) + " bold", border=0, bg='white', compound=tk.LEFT,
+#             anchor=tk.W, padx=25, activebackground='#6b84ff').pack(fill=tk.BOTH, expand=1)
             
 createHomePage()
 window.mainloop()
